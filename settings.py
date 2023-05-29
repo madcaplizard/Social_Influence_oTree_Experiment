0647d00d27ab8bdd3fab2489c376f518ae193907
madcaplizard <130039481+madcaplizard@users.noreply.github.com>
2023-05-29 23:42:24 +0100

    settings.py

--- settings.py
+++ settings.py
@@ -2,15 +2,7 @@ from os import environ
 
 
 SESSION_CONFIGS = [
-    dict(
-        name='guess_two_thirds',
-        display_name="Guess 2/3 of the Average",
-        app_sequence=['guess_two_thirds', 'payment_info'],
-        num_demo_participants=3,
-    ),
-    dict(
-        name='survey', app_sequence=['survey', 'payment_info'], num_demo_participants=1
-    ),
+  
     dict(
         name='social_influence',
         app_sequence=['social_influence'],
